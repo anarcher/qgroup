ad511ff6dd0c527f006ce67dfa5ca3a3439683f6
msshin <msshin@gracenote.com>
2017-01-20 19:19:39 +0900

    Add DoWithContext function for context support

--- qgroup.go
+++ qgroup.go
@@ -7,15 +7,21 @@ import (
 	"golang.org/x/net/context"
 )
 
-type Func func(context.Context)
+// Func has func(context.Context) and context.
+// The context uses with DoWithContext().
+type Func struct {
+	fn  func(context.Context)
+	ctx context.Context
+}
 
+//QGroup represents  function calling queues and settings
 type QGroup struct {
 	ctx        context.Context
 	cancelFunc context.CancelFunc
 	maxQueue   int
 	timeout    time.Duration
 	mu         sync.Mutex
-	q          map[string]chan Func
+	q          map[string]chan *Func
 }
 
 type QGroupOption func(g *QGroup)
@@ -35,7 +41,7 @@ func WithMaxQueue(maxQueue int) QGroupOption {
 func NewGroup(opts ...QGroupOption) *QGroup {
 	g := &QGroup{} //Default QGroup
 	g.ctx, g.cancelFunc = context.WithCancel(context.Background())
-	g.q = make(map[string]chan Func)
+	g.q = make(map[string]chan *Func)
 
 	for _, o := range opts {
 		o(g)
@@ -43,15 +49,16 @@ func NewGroup(opts ...QGroupOption) *QGroup {
 	return g
 }
 
-func (g *QGroup) Do(key string, f Func) error {
-	g.mu.Lock()
-	if _, ok := g.q[key]; !ok {
-		g.q[key] = make(chan Func, g.maxQueue)
-		go g.loopCall(key) //Calling funcs from queue
-	}
-	g.mu.Unlock()
+func (g *QGroup) Do(key string, fn func(context.Context)) error {
+	g.startLoopCall(key)
+	g.q[key] <- &Func{fn: fn}
 
-	g.q[key] <- f
+	return nil
+}
+
+func (g *QGroup) DoWithContext(key string, ctx context.Context, fn func(context.Context)) error {
+	g.startLoopCall(key)
+	g.q[key] <- &Func{fn, ctx}
 
 	return nil
 }
@@ -59,6 +66,16 @@ func (g *QGroup) Do(key string, f Func) error {
 func (g *QGroup) Cancel() error {
 	g.cancelFunc()
 	return nil
+}
+
+func (g *QGroup) startLoopCall(key string) {
+	g.mu.Lock()
+	if _, ok := g.q[key]; !ok {
+		g.q[key] = make(chan *Func, g.maxQueue)
+		go g.loopCall(key) //Calling funcs from queue
+	}
+	g.mu.Unlock()
+
 }
 
 func (g *QGroup) loopCall(key string) {
@@ -76,21 +93,34 @@ func (g *QGroup) loopCall(key string) {
 	}
 }
 
-func (g *QGroup) doCall(fn Func) {
-	var ctx context.Context
+func (g *QGroup) doCall(fn *Func) {
+	var (
+		ctx  context.Context
+		pctx context.Context
+	)
+
+	if fn.ctx != nil {
+		pctx = fn.ctx
+	} else {
+		pctx = context.Background()
+	}
 
 	if g.timeout > 0 {
-		ctx, _ = context.WithTimeout(g.ctx, g.timeout)
+		ctx, _ = context.WithTimeout(pctx, g.timeout)
 	} else {
-		ctx = context.Background()
+		ctx = pctx
 	}
 
 	c := make(chan struct{}, 1)
 
 	go func() {
-		fn(ctx)
+		fn.run(ctx)
 		c <- struct{}{}
 	}()
 
 	<-c
+}
+
+func (fn Func) run(ctx context.Context) {
+	fn.fn(ctx)
 }
